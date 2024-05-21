cabc755dd7d4a162a8a2c591606fef2329cba503
WitheringAway <124115470+notwithering@users.noreply.github.com>
2024-05-20 20:12:17 -0400

    nfl

---
+++ NFL/NFL.go
@@ -0,0 +1,251 @@
+package nfl
+
+// LOCATION >>> LOCATION
+const (
+	Arizona      string = "Arizona"
+	Atlanta      string = "Atlanta"
+	Baltimore    string = "Baltimore"
+	Buffalo      string = "Buffalo"
+	Carolina     string = "Carolina"
+	Chicago      string = "Chicago"
+	Cincinnati   string = "Cincinnati"
+	Cleveland    string = "Cleveland"
+	Dallas       string = "Dallas"
+	Denver       string = "Denver"
+	Detroit      string = "Detroit"
+	GreenBay     string = "Green Bay"
+	Houston      string = "Houston"
+	Indianapolis string = "Indianapolis"
+	Jacksonville string = "Jacksonville"
+	KansasCity   string = "Kansas City"
+	LasVegas     string = "Las Vegas"
+	LosAngeles   string = "Los Angeles"
+	Miami        string = "Miami"
+	Minnesota    string = "Minnesota"
+	NewEngland   string = "New England"
+	NewOrleans   string = "New Orleans"
+	NewYork      string = "New York"
+	Philadelphia string = "Philadelphia"
+	Pittsburgh   string = "Pittsburgh"
+	SanFrancisco string = "San Francisco"
+	Seattle      string = "Seattle"
+	TampaBay     string = "Tampa Bay"
+	Tennessee    string = "Tennessee"
+	Washington   string = "Washington"
+)
+
+// TEAM NAME >>> TEAM NAME
+const (
+	Cardinals  string = "Cardinals"
+	Falcons    string = "Falcons"
+	Ravens     string = "Ravens"
+	Bills      string = "Bills"
+	Panthers   string = "Panthers"
+	Bears      string = "Bears"
+	Bengals    string = "Bengals"
+	Browns     string = "Browns"
+	Cowboys    string = "Cowboys"
+	Broncos    string = "Broncos"
+	Lions      string = "Lions"
+	Packers    string = "Packers"
+	Texans     string = "Texans"
+	Colts      string = "Colts"
+	Jaguars    string = "Jaguars"
+	Chiefs     string = "Chiefs"
+	Raiders    string = "Raiders"
+	Chargers   string = "Chargers"
+	Rams       string = "Rams"
+	Dolphins   string = "Dolphins"
+	Vikings    string = "Vikings"
+	Patriots   string = "Patriots"
+	Saints     string = "Saints"
+	Giants     string = "Giants"
+	Jets       string = "Jets"
+	Eagles     string = "Eagles"
+	Steelers   string = "Steelers"
+	X49ers     string = "49ers"
+	Seahawks   string = "Seahawks"
+	Buccaneers string = "Buccaneers"
+	Titans     string = "Titans"
+	Redskins   string = "Redskins"
+)
+
+// FULL TEAM NAME >>> FULL TEAM NAME
+const (
+	ArizonaCardinals       string = Arizona + " " + Cardinals
+	AtlantaFalcons         string = Atlanta + " " + Falcons
+	BaltimoreRavens        string = Baltimore + " " + Ravens
+	BuffaloBills           string = Buffalo + " " + Bills
+	CarolinaPanthers       string = Carolina + " " + Panthers
+	ChicagoBears           string = Chicago + " " + Bears
+	CincinnatiBengals      string = Cincinnati + " " + Bengals
+	ClevelandBrowns        string = Cleveland + " " + Browns
+	DallasCowboys          string = Dallas + " " + Cowboys
+	DenverBroncos          string = Denver + " " + Broncos
+	DetroitLions           string = Detroit + " " + Lions
+	GreenBayPackers        string = GreenBay + " " + Packers
+	HoustonTexans          string = Houston + " " + Texans
+	IndianapolisColts      string = Indianapolis + " " + Colts
+	JacksonvilleJaguars    string = Jacksonville + " " + Jaguars
+	KansasCityChiefs       string = KansasCity + " " + Chiefs
+	LasVegasRaiders        string = LasVegas + " " + Raiders
+	LosAngelesChargers     string = LosAngeles + " " + Chargers
+	LosAngelesRams         string = LosAngeles + " " + Rams
+	MiamiDolphins          string = Miami + " " + Dolphins
+	MinnesotaVikings       string = Minnesota + " " + Vikings
+	NewEnglandPatriots     string = NewEngland + " " + Patriots
+	NewOrleansSaints       string = NewOrleans + " " + Saints
+	NewYorkGiants          string = NewYork + " " + Giants
+	NewYorkJets            string = NewYork + " " + Jets
+	PhiladelphiaEagles     string = Philadelphia + " " + Eagles
+	PittsburghSteelers     string = Pittsburgh + " " + Steelers
+	SanFrancisco49ers      string = SanFrancisco + " " + X49ers
+	SeattleSeahawks        string = Seattle + " " + Seahawks
+	TampaBayBuccaneers     string = TampaBay + " " + Buccaneers
+	TennesseeTitans        string = Tennessee + " " + Titans
+	WashingtonFootballTeam string = "Washington Football Team"
+)
+
+// TEAM NAME >>> LOCATION
+const (
+	CardinalsLocation  string = Arizona
+	FalconsLocation    string = Atlanta
+	RavensLocation     string = Baltimore
+	BillsLocation      string = Buffalo
+	PanthersLocation   string = Carolina
+	BearsLocation      string = Chicago
+	BengalsLocation    string = Cincinnati
+	BrownsLocation     string = Cleveland
+	CowboysLocation    string = Dallas
+	BroncosLocation    string = Denver
+	LionsLocation      string = Detroit
+	PackersLocation    string = GreenBay
+	TexansLocation     string = Houston
+	ColtsLocation      string = Indianapolis
+	JaguarsLocation    string = Jacksonville
+	ChiefsLocation     string = KansasCity
+	RaidersLocation    string = LasVegas
+	ChargersLocation   string = LosAngeles
+	RamsLocation       string = LosAngeles
+	DolphinsLocation   string = Miami
+	VikingsLocation    string = Minnesota
+	PatriotsLocation   string = NewEngland
+	SaintsLocation     string = NewOrleans
+	GiantsLocation     string = NewYork
+	JetsLocation       string = NewYork
+	EaglesLocation     string = Philadelphia
+	SteelersLocation   string = Pittsburgh
+	X49ersLocation     string = SanFrancisco
+	SeahawksLocation   string = Seattle
+	BuccaneersLocation string = TampaBay
+	TitansLocation     string = Tennessee
+	RedskinsLocation   string = Washington
+)
+
+// LOCATION >>> TEAM NAME
+const (
+	ArizonaTeam      string = Cardinals
+	AtlantaTeam      string = Falcons
+	BaltimoreTeam    string = Ravens
+	BuffaloTeam      string = Bills
+	CarolinaTeam     string = Panthers
+	ChicagoTeam      string = Bears
+	CincinnatiTeam   string = Bengals
+	ClevelandTeam    string = Browns
+	DallasTeam       string = Cowboys
+	DenverTeam       string = Broncos
+	DetroitTeam      string = Lions
+	GreenBayTeam     string = Packers
+	HoustonTeam      string = Texans
+	IndianapolisTeam string = Colts
+	JacksonvilleTeam string = Jaguars
+	KansasCityTeam   string = Chiefs
+	LasVegasTeam     string = Raiders
+	LosAngelesCTeam  string = Chargers
+	LosAngelesRTeam  string = Rams
+	MiamiTeam        string = Dolphins
+	MinnesotaTeam    string = Vikings
+	NewEnglandTeam   string = Patriots
+	NewOrleansTeam   string = Saints
+	NewYorkGTeam     string = Giants
+	NewYorkJTeam     string = Jets
+	PhiladelphiaTeam string = Eagles
+	PittsburghTeam   string = Steelers
+	SanFranciscoTeam string = X49ers
+	SeattleTeam      string = Seahawks
+	TampaBayTeam     string = Buccaneers
+	TennesseeTeam    string = Titans
+	WashingtonTeam   string = Redskins
+)
+
+// FULL TEAM NAME >>> LOCATION
+const (
+	ArizonaCardinalsLocation       = Arizona
+	AtlantaFalconsLocation         = Atlanta
+	BaltimoreRavensLocation        = Baltimore
+	BuffaloBillsLocation           = Buffalo
+	CarolinaPanthersLocation       = Carolina
+	ChicagoBearsLocation           = Chicago
+	CincinnatiBengalsLocation      = Cincinnati
+	ClevelandBrownsLocation        = Cleveland
+	DallasCowboysLocation          = Dallas
+	DenverBroncosLocation          = Denver
+	DetroitLionsLocation           = Detroit
+	GreenBayPackersLocation        = GreenBay
+	HoustonTexansLocation          = Houston
+	IndianapolisColtsLocation      = Indianapolis
+	JacksonvilleJaguarsLocation    = Jacksonville
+	KansasCityChiefsLocation       = KansasCity
+	LasVegasRaidersLocation        = LasVegas
+	LosAngelesChargersLocation     = LosAngeles
+	LosAngelesRamsLocation         = LosAngeles
+	MiamiDolphinsLocation          = Miami
+	MinnesotaVikingsLocation       = Minnesota
+	NewEnglandPatriotsLocation     = NewEngland
+	NewOrleansSaintsLocation       = NewOrleans
+	NewYorkGiantsLocation          = NewYork
+	NewYorkJetsLocation            = NewYork
+	PhiladelphiaEaglesLocation     = Philadelphia
+	PittsburghSteelersLocation     = Pittsburgh
+	SanFrancisco49ersLocation      = SanFrancisco
+	SeattleSeahawksLocation        = Seattle
+	TampaBayBuccaneersLocation     = TampaBay
+	TennesseeTitansLocation        = Tennessee
+	WashingtonFootballTeamLocation = Washington
+)
+
+// LOCATION >>> FULL TEAM NAME
+const (
+	ArizonaFullTeam      string = ArizonaCardinals
+	AtlantaFullTeam      string = AtlantaFalcons
+	BaltimoreFullTeam    string = BaltimoreRavens
+	BuffaloFullTeam      string = BuffaloBills
+	CarolinaFullTeam     string = CarolinaPanthers
+	ChicagoFullTeam      string = ChicagoBears
+	CincinnatiFullTeam   string = CincinnatiBengals
+	ClevelandFullTeam    string = ClevelandBrowns
+	DallasFullTeam       string = DallasCowboys
+	DenverFullTeam       string = DenverBroncos
+	DetroitFullTeam      string = DetroitLions
+	GreenBayFullTeam     string = GreenBayPackers
+	HoustonFullTeam      string = HoustonTexans
+	IndianapolisFullTeam string = IndianapolisColts
+	JacksonvilleFullTeam string = JacksonvilleJaguars
+	KansasCityFullTeam   string = KansasCityChiefs
+	LasVegasFullTeam     string = LasVegasRaiders
+	LosAngelesCFullTeam  string = LosAngelesChargers
+	LosAngelesRFullTeam  string = LosAngelesRams
+	MiamiFullTeam        string = MiamiDolphins
+	MinnesotaFullTeam    string = MinnesotaVikings
+	NewEnglandFullTeam   string = NewEnglandPatriots
+	NewOrleansFullTeam   string = NewOrleansSaints
+	NewYorkGFullTeam     string = NewYorkGiants
+	NewYorkJFullTeam     string = NewYorkJets
+	PhiladelphiaFullTeam string = PhiladelphiaEagles
+	PittsburghFullTeam   string = PittsburghSteelers
+	SanFranciscoFullTeam string = SanFrancisco49ers
+	SeattleFullTeam      string = SeattleSeahawks
+	TampaBayFullTeam     string = TampaBayBuccaneers
+	TennesseeFullTeam    string = TennesseeTitans
+	WashingtonFullTeam   string = WashingtonFootballTeam
+)
