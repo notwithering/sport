22b80d8596b2aafd1a04015412c670b712f9eac5
WitheringAway <124115470+notwithering@users.noreply.github.com>
2024-05-24 21:47:36 -0400

    nhl

---
+++ NHL/NHL.go
@@ -0,0 +1,245 @@
+package nhl
+
+// LOCATION >>> LOCATION
+const (
+	Anaheim      string = "Anaheim"
+	Arizona      string = "Arizona"
+	Boston       string = "Boston"
+	Buffalo      string = "Buffalo"
+	Calgary      string = "Calgary"
+	Carolina     string = "Carolina"
+	Chicago      string = "Chicago"
+	Colorado     string = "Colorado"
+	Columbus     string = "Columbus"
+	Dallas       string = "Dallas"
+	Detroit      string = "Detroit"
+	Edmonton     string = "Edmonton"
+	Florida      string = "Florida"
+	LosAngeles   string = "Los Angeles"
+	Minnesota    string = "Minnesota"
+	Montreal     string = "Montreal"
+	Nashville    string = "Nashville"
+	NewJersey    string = "New Jersey"
+	NewYork      string = "New York"
+	Ottawa       string = "Ottawa"
+	Philadelphia string = "Philadelphia"
+	Pittsburgh   string = "Pittsburgh"
+	SanJose      string = "San Jose"
+	StLouis      string = "St. Louis"
+	TampaBay     string = "Tampa Bay"
+	Toronto      string = "Toronto"
+	Vancouver    string = "Vancouver"
+	Vegas        string = "Vegas"
+	Washington   string = "Washington"
+	Winnipeg     string = "Winnipeg"
+)
+
+// TEAM NAME >>> TEAM NAME
+const (
+	Ducks         string = "Ducks"
+	Coyotes       string = "Coyotes"
+	Bruins        string = "Bruins"
+	Sabres        string = "Sabres"
+	Flames        string = "Flames"
+	Hurricanes    string = "Hurricanes"
+	Blackhawks    string = "Blackhawks"
+	Avalanche     string = "Avalanche"
+	BlueJackets   string = "Blue Jackets"
+	Stars         string = "Stars"
+	RedWings      string = "Red Wings"
+	Oilers        string = "Oilers"
+	Panthers      string = "Panthers"
+	Kings         string = "Kings"
+	Wild          string = "Wild"
+	Canadiens     string = "Canadiens"
+	Predators     string = "Predators"
+	Devils        string = "Devils"
+	Islanders     string = "Islanders"
+	Rangers       string = "Rangers"
+	Senators      string = "Senators"
+	Flyers        string = "Flyers"
+	Penguins      string = "Penguins"
+	Sharks        string = "Sharks"
+	Blues         string = "Blues"
+	Lightning     string = "Lightning"
+	MapleLeafs    string = "Maple Leafs"
+	Canucks       string = "Canucks"
+	GoldenKnights string = "Golden Knights"
+	Capitals      string = "Capitals"
+	Jets          string = "Jets"
+)
+
+// FULL TEAM NAME >>> FULL TEAM NAME
+const (
+	AnaheimDucks        string = Anaheim + " " + Ducks
+	ArizonaCoyotes      string = Arizona + " " + Coyotes
+	BostonBruins        string = Boston + " " + Bruins
+	BuffaloSabres       string = Buffalo + " " + Sabres
+	CalgaryFlames       string = Calgary + " " + Flames
+	CarolinaHurricanes  string = Carolina + " " + Hurricanes
+	ChicagoBlackhawks   string = Chicago + " " + Blackhawks
+	ColoradoAvalanche   string = Colorado + " " + Avalanche
+	ColumbusBlueJackets string = Columbus + " " + BlueJackets
+	DallasStars         string = Dallas + " " + Stars
+	DetroitRedWings     string = Detroit + " " + RedWings
+	EdmontonOilers      string = Edmonton + " " + Oilers
+	FloridaPanthers     string = Florida + " " + Panthers
+	LosAngelesKings     string = LosAngeles + " " + Kings
+	MinnesotaWild       string = Minnesota + " " + Wild
+	MontrealCanadiens   string = Montreal + " " + Canadiens
+	NashvillePredators  string = Nashville + " " + Predators
+	NewJerseyDevils     string = NewJersey + " " + Devils
+	NewYorkIslanders    string = NewYork + " " + Islanders
+	NewYorkRangers      string = NewYork + " " + Rangers
+	OttawaSenators      string = Ottawa + " " + Senators
+	PhiladelphiaFlyers  string = Philadelphia + " " + Flyers
+	PittsburghPenguins  string = Pittsburgh + " " + Penguins
+	SanJoseSharks       string = SanJose + " " + Sharks
+	StLouisBlues        string = StLouis + " " + Blues
+	TampaBayLightning   string = TampaBay + " " + Lightning
+	TorontoMapleLeafs   string = Toronto + " " + MapleLeafs
+	VancouverCanucks    string = Vancouver + " " + Canucks
+	VegasGoldenKnights  string = Vegas + " " + GoldenKnights
+	WashingtonCapitals  string = Washington + " " + Capitals
+	WinnipegJets        string = Winnipeg + " " + Jets
+)
+
+// TEAM NAME >>> LOCATION
+const (
+	DucksLocation         string = Anaheim
+	CoyotesLocation       string = Arizona
+	BruinsLocation        string = Boston
+	SabresLocation        string = Buffalo
+	FlamesLocation        string = Calgary
+	HurricanesLocation    string = Carolina
+	BlackhawksLocation    string = Chicago
+	AvalancheLocation     string = Colorado
+	BlueJacketsLocation   string = Columbus
+	StarsLocation         string = Dallas
+	RedWingsLocation      string = Detroit
+	OilersLocation        string = Edmonton
+	PanthersLocation      string = Florida
+	KingsLocation         string = LosAngeles
+	WildLocation          string = Minnesota
+	CanadiensLocation     string = Montreal
+	PredatorsLocation     string = Nashville
+	DevilsLocation        string = NewJersey
+	IslandersLocation     string = NewYork
+	RangersLocation       string = NewYork
+	SenatorsLocation      string = Ottawa
+	FlyersLocation        string = Philadelphia
+	PenguinsLocation      string = Pittsburgh
+	SharksLocation        string = SanJose
+	BluesLocation         string = StLouis
+	LightningLocation     string = TampaBay
+	MapleLeafsLocation    string = Toronto
+	CanucksLocation       string = Vancouver
+	GoldenKnightsLocation string = Vegas
+	CapitalsLocation      string = Washington
+	JetsLocation          string = Winnipeg
+)
+
+// LOCATION >>> TEAM NAME
+const (
+	AnaheimTeam      string = Ducks
+	ArizonaTeam      string = Coyotes
+	BostonTeam       string = Bruins
+	BuffaloTeam      string = Sabres
+	CalgaryTeam      string = Flames
+	CarolinaTeam     string = Hurricanes
+	ChicagoTeam      string = Blackhawks
+	ColoradoTeam     string = Avalanche
+	ColumbusTeam     string = BlueJackets
+	DallasTeam       string = Stars
+	DetroitTeam      string = RedWings
+	EdmontonTeam     string = Oilers
+	FloridaTeam      string = Panthers
+	LosAngelesTeam   string = Kings
+	MinnesotaTeam    string = Wild
+	MontrealTeam     string = Canadiens
+	NashvilleTeam    string = Predators
+	NewJerseyTeam    string = Devils
+	NewYorkITeam     string = Islanders
+	NewYorkRTeam     string = Rangers
+	OttawaTeam       string = Senators
+	PhiladelphiaTeam string = Flyers
+	PittsburghTeam   string = Penguins
+	SanJoseTeam      string = Sharks
+	StLouisTeam      string = Blues
+	TampaBayTeam     string = Lightning
+	TorontoTeam      string = MapleLeafs
+	VancouverTeam    string = Canucks
+	VegasTeam        string = GoldenKnights
+	WashingtonTeam   string = Capitals
+	WinnipegTeam     string = Jets
+)
+
+// FULL TEAM NAME >>> LOCATION
+const (
+	AnaheimDucksLocation        string = Anaheim
+	ArizonaCoyotesLocation      string = Arizona
+	BostonBruinsLocation        string = Boston
+	BuffaloSabresLocation       string = Buffalo
+	CalgaryFlamesLocation       string = Calgary
+	CarolinaHurricanesLocation  string = Carolina
+	ChicagoBlackhawksLocation   string = Chicago
+	ColoradoAvalancheLocation   string = Colorado
+	ColumbusBlueJacketsLocation string = Columbus
+	DallasStarsLocation         string = Dallas
+	DetroitRedWingsLocation     string = Detroit
+	EdmontonOilersLocation      string = Edmonton
+	FloridaPanthersLocation     string = Florida
+	LosAngelesKingsLocation     string = LosAngeles
+	MinnesotaWildLocation       string = Minnesota
+	MontrealCanadiensLocation   string = Montreal
+	NashvillePredatorsLocation  string = Nashville
+	NewJerseyDevilsLocation     string = NewJersey
+	NewYorkIslandersLocation    string = NewYork
+	NewYorkRangersLocation      string = NewYork
+	OttawaSenatorsLocation      string = Ottawa
+	PhiladelphiaFlyersLocation  string = Philadelphia
+	PittsburghPenguinsLocation  string = Pittsburgh
+	SanJoseSharksLocation       string = SanJose
+	StLouisBluesLocation        string = StLouis
+	TampaBayLightningLocation   string = TampaBay
+	TorontoMapleLeafsLocation   string = Toronto
+	VancouverCanucksLocation    string = Vancouver
+	VegasGoldenKnightsLocation  string = Vegas
+	WashingtonCapitalsLocation  string = Washington
+	WinnipegJetsLocation        string = Winnipeg
+)
+
+// LOCATION >>> FULL TEAM NAME
+const (
+	AnaheimFullTeam      string = AnaheimDucksLocation
+	ArizonaFullTeam      string = ArizonaCoyotesLocation
+	BostonFullTeam       string = BostonBruinsLocation
+	BuffaloFullTeam      string = BuffaloSabresLocation
+	CalgaryFullTeam      string = CalgaryFlamesLocation
+	CarolinaFullTeam     string = CarolinaHurricanesLocation
+	ChicagoFullTeam      string = ChicagoBlackhawksLocation
+	ColoradoFullTeam     string = ColoradoAvalancheLocation
+	ColumbusFullTeam     string = ColumbusBlueJacketsLocation
+	DallasFullTeam       string = DallasStarsLocation
+	DetroitFullTeam      string = DetroitRedWingsLocation
+	EdmontonFullTeam     string = EdmontonOilersLocation
+	FloridaFullTeam      string = FloridaPanthersLocation
+	LosAngelesFullTeam   string = LosAngelesKingsLocation
+	MinnesotaFullTeam    string = MinnesotaWildLocation
+	MontrealFullTeam     string = MontrealCanadiensLocation
+	NashvilleFullTeam    string = NashvillePredatorsLocation
+	NewJerseyFullTeam    string = NewJerseyDevilsLocation
+	NewYorkIFullTeam     string = NewYorkIslandersLocation
+	NewYorkRFullTeam     string = NewYorkRangersLocation
+	OttawaFullTeam       string = OttawaSenatorsLocation
+	PhiladelphiaFullTeam string = PhiladelphiaFlyersLocation
+	PittsburghFullTeam   string = PittsburghPenguinsLocation
+	SanJoseFullTeam      string = SanJoseSharksLocation
+	StLouisFullTeam      string = StLouisBluesLocation
+	TampaBayFullTeam     string = TampaBayLightningLocation
+	TorontoFullTeam      string = TorontoMapleLeafsLocation
+	VancouverFullTeam    string = VancouverCanucksLocation
+	VegasFullTeam        string = VegasGoldenKnightsLocation
+	WashingtonFullTeam   string = WashingtonCapitalsLocation
+	WinnipegFullTeam     string = WinnipegJetsLocation
+)
